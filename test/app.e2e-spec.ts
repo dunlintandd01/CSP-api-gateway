565758c17fc978e8110c6341bf3c9cecd75ed865
dunlintandd01 <dunlintan@hk01.com>
2021-05-13 18:01:03 +0800

    test: update e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,8 +17,21 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect({
+        status: 'ok',
+        info: {
+          mongoose: {
+            status: 'up',
+          },
+        },
+        error: {},
+        details: {
+          mongoose: {
+            status: 'up',
+          },
+        },
+      });
   });
 });
