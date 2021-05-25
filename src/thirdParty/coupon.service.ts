72fb901cbc0d2b7ab8a7edadccd7ae443a11c25c
George Zheng <qiaozhizheng@hk01.com>
2021-05-25 16:48:32 +0800

    feat: coupon service

---
+++ src/thirdParty/coupon.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import jwt from 'jsonwebtoken'
+import axios from 'axios'
+
+@Injectable()
+export class CouponService {
+  payload: Record<string, string>
+  opotions: Record<string, string>
+  secret: string
+  jwtToken: string
+
+  constructor(private configService: ConfigService) {
+    this.payload = {
+      clientId: this.configService.get<string>('COUPON_REDEEM_CLIENT_ID'),
+    }
+    this.secret = this.configService.get<string>('COUPON_REDEEM_SECRET')
+    this.opotions = {
+      algorithm: this.configService.get<string>('COUPON_REDEEM_ALGORITHM'),
+      expiresIn: this.configService.get<string>('COUPON_REDEEM_EXPIRES_IN'),
+    }
+    this.jwtToken = jwt.sign(this.payload, this.secret, this.opotions)
+  }
+
+  async redeemCoupon(
+    skuId: string,
+    userAccountId: string,
+  ): Promise<Record<string, string>> {
+    try {
+      const url = this.configService
+        .get<string>('COUPON_REDEEM_URL')
+        .replace('{skuId}', skuId)
+      const params = { userAccountId: userAccountId }
+      const result = await axios.post(url, params, {
+        headers: {
+          'HK01-Auth-Schema': 'API',
+          Authorization: `Bearer ${this.jwtToken}`,
+        },
+        timeout: 10000,
+      })
+      return result.data
+    } catch (error) {
+      //TODO: integrate error
+      throw new Error('coupon error')
+    }
+  }
+}
