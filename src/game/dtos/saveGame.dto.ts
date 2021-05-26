24197d07239f6232541654086ffc5848dfb05f55
pascallin <pascallin@foxmail.com>
2021-05-26 17:19:15 +0800

    refactor: save game request dto

--- src/game/dtos/saveGame.dto.ts
+++ src/game/dtos/saveGame.dto.ts
@@ -1,4 +1,10 @@
 import { OmitType } from '@nestjs/swagger'
 import { Game } from '../entities/game.entity'
 
-export class SaveGameReq extends OmitType(Game, ['id'] as const) {}
+export class SaveGameReq extends OmitType(Game, [
+  'id',
+  'createdAt',
+  'createdBy',
+  'updatedAt',
+  'updatedBy',
+] as const) {}
