6e02450b1a674bc9140471c74b4ae790430f03b8
George Zheng <qiaozhizheng@hk01.com>
2021-05-26 17:57:36 +0800

    feat: quiz entities

---
+++ src/quiz/interfaces/answer.interface.ts
@@ -0,0 +1,16 @@
+export enum ANSWER_STATUS_ENUM {
+  NORMAL = 'NORMAL',
+  DELETED = 'DELETED',
+}
+
+export enum ANSWER_TYPE_ENUM {
+  TEXT = 'TEXT',
+  IMAGE = 'IMAGE',
+}
+
+export enum ANSWER_HINT_TYPE_ENUM {
+  NONE = 'NONE',
+  IMAGE = 'IMAGE',
+  ARTICLE = 'ARTICLE',
+  VIDEO = 'VIDEO',
+}
